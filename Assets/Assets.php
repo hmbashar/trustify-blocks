77d528a0ebf73d1ccfe833d960e169d83e0c405f
hmbashar <hmbashar@gmail.com>
2025-05-06 02:00:55 +0600

    removed cdn css for swiper

--- Assets/Assets.php
+++ Assets/Assets.php
@@ -11,19 +11,6 @@ class Assets
 
     public function enqueue_editor_assets()
     {
-        wp_enqueue_style(
-            'trustify-swiper-slider',
-            'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
-            [],
-            TRUSTIFY_BLOCKS_VERSION,
-            true
-        );
-        // wp_enqueue_script(
-        //     'trustify-swiper-slider',
-        //     'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js',
-        //     [],
-        //     TRUSTIFY_BLOCKS_VERSION,
-        //     true
-        // );
+
     }
 }
