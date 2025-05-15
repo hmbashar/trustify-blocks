184317a9ebb5b2d6c41b0bbe054e4b7423c08875
hmbashar <hmbashar@gmail.com>
2025-05-15 12:12:57 +0600

    configuraton

--- blocks/build/testimonial-slide/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ba403e02ab83b1b2b43f');
--- blocks/build/testimonial-slider/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '2a086c1f0e9b2f58e348');
